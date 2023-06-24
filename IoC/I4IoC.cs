2c9916b6cc02bee34595d71eb568b0939899a2cf
laikha <laikha@gmail.com>
2023-06-24 00:38:12 -0300

    Teste de cobertura

--- IoC/I4IoC.cs
+++ IoC/I4IoC.cs
@@ -1,14 +1,20 @@
 ﻿
+using System;
+
 namespace IoC
 {
     /// <summary>
     /// A interface to use in classes
     /// </summary>
     /// @author Ricardo Silva
-    public interface I4IoC<out T>
+    public interface I4IoC<in T>
     {
         string WhoAmI();
 
+        string WhoAmI(dynamic whoIs);
+
         float GetMidGrade();
+
+        void NotInitialized(dynamic whoIs);
     }
 }
